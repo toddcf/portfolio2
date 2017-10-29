a82a40af31d37fd4db662b874662da6b773bd20b
Todd Croak-Falen <toddcf@gmail.com>
2017-10-29 16:25:08 -0700

    Improved animations for icons in ui-ux card footers.

--- assets/js/app.js
+++ assets/js/app.js
@@ -82,109 +82,159 @@ $( document ).ready( function() {
 	// Omnifood
 	$( "#omnifood" ).waypoint( function( direction ) {
 		$( "#omnifood" ).addClass( "animate-1 fadeInUp" );
-		$( "#omnifood .mini-logo-a").addClass( "animate-0-75 animate-delay-a fadeInRight");
-		$( "#omnifood .mini-logo-b").addClass( "animate-0-75 animate-delay-b fadeInRight");
-		$( "#omnifood .mini-logo-c").addClass( "animate-0-75 animate-delay-c fadeInRight");
 	}, {
-		offset: "50%"
+		offset: "85%"
 	});
 	// We actually aren't using "direction" in this case, because this animation will only happen once.
+
+	$( "#omnifood .mini-logo" ).waypoint( function( direction ) {
+		$( "#omnifood .mini-logo-a").addClass( "animate-0-75 fadeInRight");
+		$( "#omnifood .mini-logo-b").addClass( "animate-0-75 animate-delay-a fadeInRight");
+		$( "#omnifood .mini-logo-c").addClass( "animate-0-75 animate-delay-b fadeInRight");
+	}, {
+		offset: "95%"
+	});
 
 	// Author Site
 	$( "#author-site" ).waypoint( function( direction ) {
 		$( "#author-site" ).addClass( "animate-1 fadeInUp" );
-		$( "#author-site .mini-logo-a").addClass( "animate-0-75 animate-delay-a fadeInRight");
-		$( "#author-site .mini-logo-b").addClass( "animate-0-75 animate-delay-b fadeInRight");
-		$( "#author-site .mini-logo-c").addClass( "animate-0-75 animate-delay-c fadeInRight");
-		$( "#author-site .mini-logo-d").addClass( "animate-0-75 animate-delay-d fadeInRight");
-		$( "#author-site .mini-logo-e").addClass( "animate-0-75 animate-delay-e fadeInRight");
 	}, {
-		offset: "50%"
+		offset: "85%"
+	});
+
+	$( "#author-site .mini-logo" ).waypoint( function( direction ) {
+		$( "#author-site .mini-logo-a").addClass( "animate-0-75 fadeInRight");
+		$( "#author-site .mini-logo-b").addClass( "animate-0-75 animate-delay-a fadeInRight");
+		$( "#author-site .mini-logo-c").addClass( "animate-0-75 animate-delay-b fadeInRight");
+		$( "#author-site .mini-logo-d").addClass( "animate-0-75 animate-delay-c fadeInRight");
+		$( "#author-site .mini-logo-e").addClass( "animate-0-75 animate-delay-d fadeInRight");
+	}, {
+		offset: "95%"
 	});
 
 	// Piel Canela
 	$( "#piel-canela" ).waypoint( function( direction ) {
 		$( "#piel-canela" ).addClass( "animate-1 fadeInUp" );
-		$( "#piel-canela .mini-logo-a").addClass( "animate-0-75 animate-delay-a fadeInRight");
-		$( "#piel-canela .mini-logo-b").addClass( "animate-0-75 animate-delay-b fadeInRight");
-		$( "#piel-canela .mini-logo-c").addClass( "animate-0-75 animate-delay-c fadeInRight");
-		$( "#piel-canela .mini-logo-d").addClass( "animate-0-75 animate-delay-d fadeInRight");
 	}, {
-		offset: "50%"
+		offset: "85%"
+	});
+
+	$( "#piel-canela .mini-logo" ).waypoint( function( direction ) {
+		$( "#piel-canela .mini-logo-a").addClass( "animate-0-75 fadeInRight");
+		$( "#piel-canela .mini-logo-b").addClass( "animate-0-75 animate-delay-a fadeInRight");
+		$( "#piel-canela .mini-logo-c").addClass( "animate-0-75 animate-delay-b fadeInRight");
+		$( "#piel-canela .mini-logo-d").addClass( "animate-0-75 animate-delay-c fadeInRight");
+	}, {
+		offset: "95%"
 	});
 
 	// My Portfolio
 	$( "#my-portfolio" ).waypoint( function( direction ) {
 		$( "#my-portfolio" ).addClass( "animate-1 fadeInUp" );
-		$( "#my-portfolio .mini-logo-a").addClass( "animate-0-75 animate-delay-a fadeInRight");
-		$( "#my-portfolio .mini-logo-b").addClass( "animate-0-75 animate-delay-b fadeInRight");
-		$( "#my-portfolio .mini-logo-c").addClass( "animate-0-75 animate-delay-c fadeInRight");
-		$( "#my-portfolio .mini-logo-d").addClass( "animate-0-75 animate-delay-d fadeInRight");
-		$( "#my-portfolio .mini-logo-e").addClass( "animate-0-75 animate-delay-e fadeInRight");
 	}, {
-		offset: "50%"
+		offset: "85%"
+	});
+
+	$( "#my-portfolio .mini-logo" ).waypoint( function( direction ) {
+		$( "#my-portfolio .mini-logo-a").addClass( "animate-0-75 fadeInRight");
+		$( "#my-portfolio .mini-logo-b").addClass( "animate-0-75 animate-delay-a fadeInRight");
+		$( "#my-portfolio .mini-logo-c").addClass( "animate-0-75 animate-delay-b fadeInRight");
+		$( "#my-portfolio .mini-logo-d").addClass( "animate-0-75 animate-delay-c fadeInRight");
+		$( "#my-portfolio .mini-logo-e").addClass( "animate-0-75 animate-delay-d fadeInRight");
+	}, {
+		offset: "95%"
 	});
 
 	// Image Gallery
 	$( "#image-gallery" ).waypoint( function( direction ) {
 		$( "#image-gallery" ).addClass( "animate-1 fadeInUp" );
-		$( "#image-gallery .mini-logo-a").addClass( "animate-0-75 animate-delay-a fadeInRight");
-		$( "#image-gallery .mini-logo-b").addClass( "animate-0-75 animate-delay-b fadeInRight");
 	}, {
-		offset: "50%"
+		offset: "85%"
+	});
+
+	$( "#image-gallery .mini-logo" ).waypoint( function( direction ) {
+		$( "#image-gallery .mini-logo-a").addClass( "animate-0-75 fadeInRight");
+		$( "#image-gallery .mini-logo-b").addClass( "animate-0-75 animate-delay-a fadeInRight");
+	}, {
+		offset: "95%"
 	});
 
 	// Transport Image Gallery
 	$( "#transport" ).waypoint( function( direction ) {
 		$( "#transport" ).addClass( "animate-1 fadeInUp" );
-		$( "#transport .mini-logo-a").addClass( "animate-0-75 animate-delay-a fadeInRight");
-		$( "#transport .mini-logo-b").addClass( "animate-0-75 animate-delay-b fadeInRight");
-		$( "#transport .mini-logo-c").addClass( "animate-0-75 animate-delay-c fadeInRight");
-		$( "#transport .mini-logo-d").addClass( "animate-0-75 animate-delay-d fadeInRight");
 	}, {
-		offset: "50%"
+		offset: "85%"
+	});
+
+	$( "#transport .mini-logo" ).waypoint( function( direction ) {
+		$( "#transport .mini-logo-a").addClass( "animate-0-75 fadeInRight");
+		$( "#transport .mini-logo-b").addClass( "animate-0-75 animate-delay-a fadeInRight");
+		$( "#transport .mini-logo-c").addClass( "animate-0-75 animate-delay-b fadeInRight");
+		$( "#transport .mini-logo-d").addClass( "animate-0-75 animate-delay-c fadeInRight");
+	}, {
+		offset: "95%"
 	});
 
 	// Purrfect Match
 	$( "#purrfect" ).waypoint( function( direction ) {
 		$( "#purrfect" ).addClass( "animate-1 fadeInUp" );
-		$( "#purrfect .mini-logo-a").addClass( "animate-0-75 animate-delay-a fadeInRight");
-		$( "#purrfect .mini-logo-b").addClass( "animate-0-75 animate-delay-b fadeInRight");
-		$( "#purrfect .mini-logo-c").addClass( "animate-0-75 animate-delay-c fadeInRight");
-		$( "#purrfect .mini-logo-d").addClass( "animate-0-75 animate-delay-d fadeInRight");
 	}, {
-		offset: "50%"
+		offset: "85%"
+	});
+
+	$( "#purrfect .mini-logo" ).waypoint( function( direction ) {
+		$( "#purrfect .mini-logo-a").addClass( "animate-0-75 fadeInRight");
+		$( "#purrfect .mini-logo-b").addClass( "animate-0-75 animate-delay-a fadeInRight");
+		$( "#purrfect .mini-logo-c").addClass( "animate-0-75 animate-delay-b fadeInRight");
+		$( "#purrfect .mini-logo-d").addClass( "animate-0-75 animate-delay-c fadeInRight");
+	}, {
+		offset: "95%"
 	});
 
 	// Teleportation Vacation
 	$( "#teleportation" ).waypoint( function( direction ) {
 		$( "#teleportation" ).addClass( "animate-1 fadeInUp" );
-		$( "#teleportation .mini-logo-a").addClass( "animate-0-75 animate-delay-a fadeInRight");
-		$( "#teleportation .mini-logo-b").addClass( "animate-0-75 animate-delay-b fadeInRight");
-		$( "#teleportation .mini-logo-c").addClass( "animate-0-75 animate-delay-c fadeInRight");
 	}, {
-		offset: "50%"
+		offset: "85%"
+	});
+
+	$( "#teleportation .mini-logo" ).waypoint( function( direction ) {
+		$( "#teleportation .mini-logo-a").addClass( "animate-0-75 fadeInRight");
+		$( "#teleportation .mini-logo-b").addClass( "animate-0-75 animate-delay-a fadeInRight");
+		$( "#teleportation .mini-logo-c").addClass( "animate-0-75 animate-delay-b fadeInRight");
+	}, {
+		offset: "95%"
 	});
 
 	// New York Advertising Agency
 	$( "#ny-ad-agency" ).waypoint( function( direction ) {
 		$( "#ny-ad-agency" ).addClass( "animate-1 fadeInUp" );
-		$( "#ny-ad-agency .mini-logo-a").addClass( "animate-0-75 animate-delay-a fadeInRight");
-		$( "#ny-ad-agency .mini-logo-b").addClass( "animate-0-75 animate-delay-b fadeInRight");
-		$( "#ny-ad-agency .mini-logo-c").addClass( "animate-0-75 animate-delay-c fadeInRight");
-		$( "#ny-ad-agency .mini-logo-d").addClass( "animate-0-75 animate-delay-d fadeInRight");
 	}, {
-		offset: "50%"
+		offset: "85%"
+	});
+
+	$( "#ny-ad-agency .mini-logo" ).waypoint( function( direction ) {
+		$( "#ny-ad-agency .mini-logo-a").addClass( "animate-0-75 fadeInRight");
+		$( "#ny-ad-agency .mini-logo-b").addClass( "animate-0-75 animate-delay-a fadeInRight");
+		$( "#ny-ad-agency .mini-logo-c").addClass( "animate-0-75 animate-delay-b fadeInRight");
+		$( "#ny-ad-agency .mini-logo-d").addClass( "animate-0-75 animate-delay-c fadeInRight");
+	}, {
+		offset: "95%"
 	});
 
 	// Nature Escape
 	$( "#nature-escape" ).waypoint( function( direction ) {
 		$( "#nature-escape" ).addClass( "animate-1 fadeInUp" );
-		$( "#nature-escape .mini-logo-a").addClass( "animate-0-75 animate-delay-a fadeInRight");
-		$( "#nature-escape .mini-logo-b").addClass( "animate-0-75 animate-delay-b fadeInRight");
-		$( "#nature-escape .mini-logo-c").addClass( "animate-0-75 animate-delay-c fadeInRight");
 	}, {
-		offset: "50%"
+		offset: "85%"
+	});
+
+	$( "#nature-escape .mini-logo" ).waypoint( function( direction ) {
+		$( "#nature-escape .mini-logo-a").addClass( "animate-0-75 fadeInRight");
+		$( "#nature-escape .mini-logo-b").addClass( "animate-0-75 animate-delay-a fadeInRight");
+		$( "#nature-escape .mini-logo-c").addClass( "animate-0-75 animate-delay-b fadeInRight");
+	}, {
+		offset: "95%"
 	});
 
 	// Wealth Tracker
